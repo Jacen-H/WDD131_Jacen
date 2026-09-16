1434c7f01edd970247c1303b888f0a1c9617b58e
Jacen-H <haw24004@byui.edu>
2026-09-16 14:32:25 -0600

    Add files via upload

---
+++ ponder/variables.js
@@ -0,0 +1,13 @@
+
+// declare a variable
+let x = 5;
+let age = 23, name = "John Doe";
+let uyguguyguyguygyuguyguyguyguyguyguyg 
+= 1;
+
+// console.log is similar to print in python
+console.log(age);
+
+// a constant variable. these cannot be changed after being declared
+const name2 = "Jane Doe";
+
