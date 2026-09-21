3cc3a0e6f56b3f6d24001bdd55953405dfb196c0
Jacen-H <haw24004@byui.edu>
2026-09-21 13:56:46 -0600

    finished week 1 assignments

--- ponder/variables.js
+++ ponder/variables.js
@@ -11,3 +11,16 @@ console.log(age);
 // a constant variable. these cannot be changed after being declared
 const name2 = "Jane Doe";
 
+// Scope = where a variable can be referenced by name
+
+if(age == 22)
+{
+    // a new scope
+    console.log(name2);
+    let favoriteColor = "blue";
+}
+
+// console.log(favoriteColor); 
+// This won't work, because favoriteColor is defined in a smaller scope.
+
+document.querySelector("h1").style.color = "blue";
